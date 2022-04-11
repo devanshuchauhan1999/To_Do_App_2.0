82b686ac7b26b3d641ec44c0a2fc58f5c7ce99e0
devanshuchauhan1999 <chauhandevanshu121@gmail.com>
2022-04-11 18:53:01 +0530

    Daggger Hilt setup started

---
+++ app/src/main/java/com/devanshu/todoapp20/ToDoApplication.kt
@@ -0,0 +1,8 @@
+package com.devanshu.todoapp20
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication:Application() {
+}
---
+++ app/src/main/java/com/devanshu/todoapp20/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.devanshu.todoapp20.di
+
+import android.content.Context
+import androidx.room.Room
+import com.devanshu.todoapp20.data.models.ToDoDatabase
+import com.devanshu.todoapp20.utils.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext
+        context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
