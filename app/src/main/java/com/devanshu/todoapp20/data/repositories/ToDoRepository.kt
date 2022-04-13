147f66337ba92bdad38b58c02b763e897cc0c7a1
devanshuchauhan1999 <chauhandevanshu121@gmail.com>
2022-04-13 15:43:21 +0530

    view model setup done

--- app/src/main/java/com/devanshu/todoapp20/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/devanshu/todoapp20/data/repositories/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.devanshu.todoapp20.data.repositories
 
 import com.devanshu.todoapp20.data.ToDoDao
 import com.devanshu.todoapp20.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
