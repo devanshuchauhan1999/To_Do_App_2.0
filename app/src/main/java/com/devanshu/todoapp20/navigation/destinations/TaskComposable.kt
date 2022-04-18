cc24e9e6528b0c134a57869b4be54f117e9babb8
devanshuchauhan1999 <chauhandevanshu121@gmail.com>
2022-04-18 12:58:49 +0530

    Task Screen top bar default created

--- app/src/main/java/com/devanshu/todoapp20/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/devanshu/todoapp20/navigation/destinations/TaskComposable.kt
@@ -1,11 +1,13 @@
 package com.devanshu.todoapp20.navigation.destinations
 
+import android.util.Log
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.devanshu.todoapp20.utils.Action
 import com.devanshu.todoapp20.utils.Constants
+import com.devanshu.todoapp20.utils.Constants.TASK_ARGUMENT_KEY
 import com.devanshu.todoapp20.utils.Constants.TASK_SCREEN
 
 fun NavGraphBuilder.taskComposable(
@@ -16,7 +18,8 @@ fun NavGraphBuilder.taskComposable(
         arguments = listOf(navArgument(Constants.LIST_ARGUMENT_KEY){
             type = NavType.IntType
         })
-    ){
-
+    ){ navBackStackEntry ->
+        val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
+        Log.d("TaskComposable",taskId.toString())
     }
 }
---
+++ app/src/main/java/com/devanshu/todoapp20/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,17 @@
+package com.devanshu.todoapp20.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen(){
+
+    Scaffold(
+        topBar = {
+
+        },
+        content = {
+
+        }
+    )
+}
